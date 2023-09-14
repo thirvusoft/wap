ba153cd8592c2dcd87061947348b2b3154e918c9
creative-paramu <pparameshwari@thirvusoft.in>
2023-09-14 23:10:53 +0530

    wap_docs

---
+++ wap/whatsapp_integration___int/doctype/wap_api_log/wap_api_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Thirvusoft Private Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('WAP API Log', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ wap/whatsapp_integration___int/doctype/wap_provider/wap_provider.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Thirvusoft Private Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('WAP Provider', {
+	// refresh: function(frm) {
+
+	// }
+});
